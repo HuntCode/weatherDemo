11c41ec8b4f971cacb45a31eb7c2bed1d410075a
xyqsh <672562760@qq.com>
2018-04-22 22:46:16 +0800

    加入创建数据库和表的配置

---
+++ app/src/main/java/com/example/weatherdemo/db/County.java
@@ -0,0 +1,10 @@
+package com.example.weatherdemo.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by admin on 2018/4/22.
+ */
+
+public class County extends DataSupport {
+}
---
+++ app/src/main/java/com/example/weatherdemo/db/Province.java
@@ -0,0 +1,8 @@
+package com.example.weatherdemo.db;
+
+/**
+ * Created by admin on 2018/4/22.
+ */
+
+public class Province {
+}
