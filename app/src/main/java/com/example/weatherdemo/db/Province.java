7d8c0a0952c26f83ad5220728203cbed95afcbdd
xyqsh <672562760@qq.com>
2018-04-23 15:15:53 +0800

    完成遍历省市县列表功能

--- app/src/main/java/com/example/weatherdemo/db/Province.java
+++ app/src/main/java/com/example/weatherdemo/db/Province.java
@@ -1,8 +1,33 @@
 package com.example.weatherdemo.db;
 
-/**
- * Created by admin on 2018/4/22.
- */
+import org.litepal.crud.DataSupport;
 
-public class Province {
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode(){
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode){
+        this.provinceCode = provinceCode;
+    }
 }
